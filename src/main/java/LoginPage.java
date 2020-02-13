bc57aef3eb2bae878c7ae21c7cca2bb3fd63b78f
MxProGG <imxprogg@gmail.com>
2020-02-13 18:07:31 +0300

    Тест страницы LoginPage - Junit (1)

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.Set;
 
@@ -10,51 +11,72 @@ public class LoginPage {
     public LoginPage (WebDriver driver){
         this.driver = driver;
     }
-    //private WebElement login;
-    //private WebElement pass;
 
+   /* @FindBy (xpath = "//input[@id='Login']")
+    private WebElement loginInput;*/
     private By loginInput = By.xpath("//input[@id='Login']");
-    private By passwordInput = By.xpath("//input[@id='Password']");
-    private By buttonLogin = By.xpath("//input[@id='loginBtn']");
-    private By checkBoxRemember = By.xpath("//input[@id='Remember']");
-    private By linkHelp = By.xpath("//a [@href ='http://confluence.softrust.ru/pages/viewpage.action?pageId=7406016']");
-    private By heading = By.xpath("//div[text()=\"TrustMed\"]");
-    private By errorLogin = By.xpath("//span[@class='error-login']");
 
-    public MainPage clickLogin () {
-        driver.findElement(buttonLogin).click();
-        return new MainPage(driver);
-    }
+    /*@FindBy (xpath = "//input[@id='Password']")
+    private WebElement passwordInput;*/
+    private By passwordInput = By.xpath("//input[@id='Password']");
+
+
+    /*@FindBy (xpath = "//input[@id='loginBtn']")
+    private WebElement buttonLogin;*/
+    private By buttonLogin = By.xpath("//input[@id='loginBtn']");
+
+
+    @FindBy (xpath = "//input[@id='Remember']")
+    private WebElement checkBoxRemember;
+
+    @FindBy (xpath = "//a [@href ='http://confluence.softrust.ru/pages/viewpage.action?pageId=7406016']")
+    private WebElement linkHelp;
+
+    @FindBy (xpath = "//div[text()=\"TrustMed\"]")
+    private WebElement heading;
+
+    @FindBy (xpath = "//span[@class='error-login']")
+    private WebElement errorLogin;
+
 
     public LoginPage typeLogin (String username) {
         driver.findElement(loginInput).sendKeys(username);
+        //loginInput.sendKeys(username);
         return this;
     }
 
     public LoginPage typePassword (String password) {
         driver.findElement(passwordInput).sendKeys(password);
+        //passwordInput.sendKeys(password);
         return this;
     }
 
+    public MainPage clickLogin () {
+        driver.findElement(buttonLogin).click();
+        //buttonLogin.click();
+        return new MainPage(driver);
+    }
+
     public void clickRemember () {
-        if (!driver.findElement(checkBoxRemember).isSelected()){
-        driver.findElement(checkBoxRemember).click();}
+        if (!checkBoxRemember.isSelected()){
+        checkBoxRemember.click();}
     }
 
     public void clickLinkHelp () {
-        driver.findElement(linkHelp).click();
+        linkHelp.click();
+
     }
 
     public String getHeadingText(){
-        return driver.findElement(heading).getText();
+        return heading.getText();
     }
 
-    public String getErorrText() { return driver.findElement(errorLogin).getText(); }
+    public String getErorrText() { return errorLogin.getText(); }
 
     public MainPage entrySystem (String username, String password ){
-        String mainWindow = driver.getWindowHandle();
-        this.clickLinkHelp();
-        driver.switchTo().window(mainWindow);
+        //String mainWindow = driver.getWindowHandle();
+        //this.clickLinkHelp();
+        //driver.switchTo().window(mainWindow);
         this.typeLogin(username);
         this.typePassword(password);
         this.clickRemember();
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,18 +1,23 @@
-import org.openqa.selenium.By;
+import org.junit.*;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainClass {
 
-       public static void main(String[] args) {
-        /*//Путь к веб драйверу
+
+public class LoginPageTest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @Before
+    public void setUp(){
+        //Путь к веб драйверу
         System.setProperty("webdriver.chrome.driver", "D:\\java_project\\java_lesson_selenium\\drives\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         //Неявное ожидание для все элементов 10 сек
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //Чтоб окно браузера запускалось на втором мониторе
@@ -20,15 +25,27 @@ public class MainClass {
         //На весь экран
         driver.manage().window().maximize();
         //driver.manage().window().setSize(new Dimension(1600,1000));
-
         driver.get("http://192.168.7.54/mis/test2/");
-        //LoginPage loginPage = new LoginPage(driver);
-        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        loginPage.entrySystem("admin","11");
-
-        MainPage mainPage = new MainPage(driver);
-        mainPage.linkPage("Журнал направлений на МСЭ");
-
-        driver.quit();*/
+        loginPage = new LoginPage(driver);
+        //LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
     }
+
+//    @Test
+//    public  void helpClick (){
+//        loginPage.clickLinkHelp();
+//
+//    }
+
+    @Test
+    public void loginErrorTest(){
+        MainPage mainPage = loginPage.entrySystem("admin","11");
+        String error = loginPage.getErorrText();
+        Assert.assertEquals("Неверный пароль пользователя",error);
+    }
+
+    /*@After
+    public void tearDown(){
+        driver.quit();
+    }*/
+
 }
