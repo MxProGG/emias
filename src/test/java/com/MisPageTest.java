d950a7101e6c903f923279326704e5e49dc48679
MxProGG <imxprogg@gmail.com>
2020-06-05 01:21:22 +0300

    allure и переходим на junit5_____

---
+++ src/test/java/com/MisPageTest.java
@@ -0,0 +1,69 @@
+package com;
+
+import com.page.LoginPage;
+import com.page.MainPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static java.lang.Thread.sleep;
+
+
+public class MisPageTest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private MainPage mainPage;
+
+
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverInstall driverInstall = new WebDriverInstall();
+        driver = driverInstall.setUpDriver();
+        loginPage = new LoginPage(driver);
+        //com.page.LoginPage loginPage = PageFactory.initElements(driver, com.page.LoginPage.class);
+    }
+
+    @Test
+    public  void helpClick (){
+        Assertions.assertEquals("TrustMed",loginPage.getHeadingText());
+        loginPage.clickLinkHelp();
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+        Assertions.assertEquals("https://confluence.softrust.ru/pages/viewpage.action?pageId=7405997",driver.getCurrentUrl());
+     }
+
+    @Test
+    public void loginErrorTest(){
+        loginPage.entrySystem("admin","22");
+        String error = loginPage.getErorrText();
+        Assertions.assertEquals("Неверный пароль пользователя",error);
+    }
+
+    @Test
+    public void loginSuccesful(){
+        loginPage.entrySystem("admin","11");
+        Assertions.assertEquals("Центр управления", driver.getTitle()) ;
+    }
+
+    @Test
+    public void loginOut () throws InterruptedException {
+        loginPage.entrySystem("admin","11");
+        mainPage = new MainPage(driver);
+        sleep(2000);
+        mainPage.logOut();
+        Assertions.assertEquals("TrustMed", loginPage.getHeadingText());
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+        driver.quit();
+    }
+
+}
